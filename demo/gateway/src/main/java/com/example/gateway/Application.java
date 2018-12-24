58a44fc88c6d4580bc34c6dfa65bdc1a76c5389f
Yuly Huang <592443616@qq.com>
2018-12-24 20:36:25 +0800

    update

--- demo/gateway/src/main/java/com/example/gateway/Application.java
+++ demo/gateway/src/main/java/com/example/gateway/Application.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableZuulProxy
 @ComponentScan("com.example")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
